8797f37870e250e3544f6bec86c568b7cd023377
c0r73x <c0r73x@gmail.com>
2026-03-09 15:07:48 +0100

    Minified lua

--- lua/neotags/languages/yue.lua
+++ lua/neotags/languages/yue.lua
@@ -1,1 +1,25 @@
-local b={'vim.*','.*Comment.*','.*String.*','yueKeyword','yueObjAssign'}return{order='cifmspv',kinds={c={group='neotags_ClassTag',notin=b},i={group='neotags_ImportTag',notin=b},f={group='neotags_FunctionTag',notin=b},m={group='neotags_FunctionTag',notin=b},v={group='neotags_VariableTag',allow_keyword=false,notin=b},s={group='neotags_EnumTag',notin=b},p={group='neotags_TypeTag',allow_keyword=false,notin=b}}}
+local notin = { 
+'vim.*', 
+'.*Comment.*', 
+'.*String.*', 
+'yueKeyword', 
+'yueObjAssign' }return { order = 
+
+
+
+'cifmspv', kinds = { c = { group = 
+
+'neotags_ClassTag', notin = notin }, i = { group = 
+'neotags_ImportTag', notin = notin }, f = { group = 
+'neotags_FunctionTag', notin = notin }, m = { group = 
+'neotags_FunctionTag', notin = notin }, v = { group = 
+
+'neotags_VariableTag', allow_keyword = 
+false, notin = 
+notin }, s = { group = 
+
+'neotags_EnumTag', notin = notin }, p = { group = 
+
+'neotags_TypeTag', allow_keyword = 
+false, notin = 
+notin } } }
